d4ae46bdbfc921536966bcb338a3b867ecac8dfc
ivanmeler <i_ivan@windowslive.com>
2021-12-14 09:45:41 +0000

    Revert "Sealions permissive"
    This reverts commit 1ae403526cbab6745686bf73ba26671b3d93aba6.

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -155,7 +155,6 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	selnl_notify_setenforce(new_value);
 	selinux_status_update_setenforce(new_value);
 #else
-	new_value = 0;
 	if (new_value != selinux_enforcing) {
 		length = avc_has_perm(current_sid(), SECINITSID_SECURITY,
 				      SECCLASS_SECURITY, SECURITY__SETENFORCE,
